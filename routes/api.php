4a688ad81a55ac4ab1e5053e3a2e886902b416ee
MCNGN <rifqi.adibbahagia@binus.ac.id>
2025-01-19 17:26:56 +0700

    Add Forum Controllers

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EducationController;
+use App\Http\Controllers\ForumController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
